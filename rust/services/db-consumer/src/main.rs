1205e260ca71142a93e4ccbefc685406b53e348b
Otávio Henrique <otaviocap@hotmail.com.br>
2023-07-26 23:54:32 -0300

    Almost finishing rust code 2

--- rust/services/db-consumer/src/main.rs
+++ rust/services/db-consumer/src/main.rs
@@ -1,27 +1,32 @@
 mod user;
 
-use sqlx::postgres::PgPoolOptions;
-use user::User;
+use std::str::FromStr;
+
+use sqlx::{postgres::PgPoolOptions, types::Uuid};
+
+use crate::user::Users;
 
 #[tokio::main]
 async fn main() {
+
+    println!("RUST => Db Consumer running!");
 
     dotenv::dotenv().ok();
 
     let pool = PgPoolOptions::new()
         .max_connections(5)
-        .connect(std::env::var("DATABASE_URL").expect("DATABASE_URL must be set.").as_str())
+        .connect(
+            std::env::var("DATABASE_URL")
+            .expect("DATABASE_URL must be set.")
+            .as_str()
+        )
         .await
         .expect("Error connecting into the databse!");
 
-    let rows = sqlx::query_as!(User, r#"--sql
-        SELECT * FROM users;
-    "#)
-    .fetch_all(&pool)
-    .await
-    .expect("Something went wrong while querying users");
+    let users = Users::new(pool);
 
-    println!("{:?}", rows)
+    println!("{:?}", users.getAllUsers().await);
+    println!("{:?}", users.getUserById(Uuid::parse_str("aa304752-2bf7-11ee-8732-0242ac140002").unwrap()).await);
 
 
 }
--- rust/services/db-consumer/src/user.rs
+++ rust/services/db-consumer/src/user.rs
@@ -1,9 +1,57 @@
-use sqlx::types::Uuid;
+use sqlx::{types::Uuid, Pool, Postgres};
 
 #[derive(sqlx::FromRow, Debug)]
 pub struct User {
     pub username: String,
     pub password: String,
     pub id: Uuid,
-    pub status: Option<String>,
+}
+
+pub struct Users {
+    pool: Pool<Postgres>
+}
+
+impl Users {
+
+    pub fn new(pool: Pool<Postgres>) -> Users {
+        Users { pool }
+    }
+
+    pub async fn getAllUsers(&self) -> Vec<User> {
+        sqlx::query_as!(User, r#"--sql
+            SELECT * FROM users;
+        "#)
+        .fetch_all(&self.pool)
+        .await
+        .expect("Something went wrong while querying users")
+    }
+
+    pub async fn getUserById(&self, id: Uuid) -> User{
+        sqlx::query_as!(User, r#"--sql
+            SELECT 
+                username,
+                password,
+                id
+            FROM users
+            WHERE id = $1
+        "#, id)
+        .fetch_one(&self.pool)
+        .await
+        .expect(format!("Something went wrong while querying user with id: {:?}", id).as_str()) 
+    }
+
+    pub async fn addUser(&self, user: User) -> Result<Uuid, ()>{
+        let rec = sqlx::query!(r#"--sql
+            INSERT INTO users (username, password)
+            VALUES ($1, $2)
+            RETURNING id
+        "#, user.username, user.password)
+        .fetch_one(&self.pool)
+        .await
+        .expect("Something went wrong while querying users");
+
+        Ok(rec.id)
+    }
+
+
 }
